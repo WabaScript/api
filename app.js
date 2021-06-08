ad78b268f621c67511418c7d09b2f611ac89a537
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-08 19:48:36 +0200

    added auth routes

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const fastify = require("fastify");
 const { debug } = require("./routes/debug.js");
 const { admin } = require("./routes/admin.js");
+const { auth } = require("./routes/auth.js");
 
 const build = (opts = {}) => {
   const app = fastify(opts);
 
   app.register(debug);
   app.register(admin, { prefix: "/v2" });
+  app.register(auth, { prefix: "/v2/auth" });
   // app.register(debug, { prefix: "/" });
 
   return app;

edd51ed64c32c16ddda64cfcf46997abcdb2e42a
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-20 16:16:33 +0200

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { build } = require("./app.js");
 
 const app = build({ logger: true });
 
-app.listen(3000, (err, address) => {
+app.listen(5000, (err, address) => {
   if (err) {
     console.log(err);
     process.exit(1);

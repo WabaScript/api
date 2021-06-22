9adb524593fcc780d9eef0cfb5e67826f92268be
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-22 23:54:08 +0200

    added ports binding

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { build } = require("./app.js");
 
 const app = build({ logger: true });
 
-app.listen(5000, (err, address) => {
+app.listen(5000, "0.0.0.0", (err, address) => {
   if (err) {
     console.log(err);
     process.exit(1);

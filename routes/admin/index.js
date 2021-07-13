ed08dd1ea447dd30074e4a2ef745c38850e91b48
Renato Pozzi <r.pozzi@bluecube.it>
2021-07-13 16:29:43 +0200

    initial db module

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,22 +1,21 @@
-const { Website } = require("../../lib/models");
-const { User } = require("../../lib/models");
-const { Browser } = require("../../lib/models");
+const { getAllBrowsers, getAllWebsites, getAllUsers } = require("../../lib/db");
 
 const admin = (fastify, _, done) => {
   fastify.addHook("onRequest", (request) => request.jwtVerify());
 
-  fastify.get("/websites", getWebsites);
-  fastify.get("/users", getUsers);
-  fastify.get("/browsers", getBrowsers);
+  fastify.get("/websites", async () => {
+    return await getAllWebsites();
+  });
+
+  fastify.get("/users", async () => {
+    return await getAllUsers();
+  });
+
+  fastify.get("/browsers", async () => {
+    return await getAllBrowsers();
+  });
 
   done();
 };
-
-const getWebsites = async (_request, _reply) =>
-  new Website().fetchAll({ withRelated: ["user"] });
-
-const getUsers = async (_request, _reply) => new User().fetchAll();
-
-const getBrowsers = async (_request, _reply) => new Browser().fetchAll();
 
 module.exports = { admin };

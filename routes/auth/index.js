4df8421eaab9fefc8e748e8de4734d113f3cf4c9
Renato Pozzi <r.pozzi@bluecube.it>
2021-07-13 16:40:00 +0200

    refactor auth apis & add getUserByEmail

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,57 +1,48 @@
-const { User } = require("../../lib/models");
 const { verify } = require("../../utils/hash");
+const { getUserByEmail } = require("../../lib/db");
 const { AUTH_COOKIE, AUTH_COOKIE_LIFETIME } = require("../../utils/constants");
 
+const cookieConf = {
+  // domain: "your.domain",
+  path: "/",
+  secure: false,
+  httpOnly: true,
+  sameSite: true,
+  maxAge: AUTH_COOKIE_LIFETIME,
+};
+
 const auth = (fastify, _opts, done) => {
-  fastify.post("/login", postLogin);
-  fastify.post("/logout", postLogout);
+  fastify.post("/login", async (request, reply) => {
+    const user = await login({ ...request.body });
+
+    if (user) {
+      const accessToken = await reply.jwtSign({ data: user });
+      const responseType = process.env.AUTH_MODE || "jwt";
+
+      reply
+        .setCookie(AUTH_COOKIE, accessToken, cookieConf)
+        .status(200)
+        .send({ response_type: responseType, access_token: accessToken });
+    }
+
+    return reply.status(401).send({ message: "Unauthorized" });
+  });
+
+  fastify.post("/logout", (_, reply) => {
+    reply
+      .setCookie(AUTH_COOKIE, null, {
+        ...cookieConf,
+        maxAge: -1,
+      })
+      .status(200)
+      .send({ message: "Logout succeded." });
+  });
 
   done();
 };
 
-const postLogin = async (request, reply) => {
-  const { email, password } = request.body;
-
-  const user = await login({ email, password });
-
-  if (user) {
-    const accessToken = await reply.jwtSign({ data: user });
-
-    const responseType = process.env.AUTH_MODE || "jwt";
-
-    reply
-      .setCookie(AUTH_COOKIE, accessToken, {
-        // domain: "your.domain",
-        path: "/",
-        secure: false,
-        httpOnly: true,
-        sameSite: true,
-        maxAge: AUTH_COOKIE_LIFETIME,
-      })
-      .status(200)
-      .send({ response_type: responseType, access_token: accessToken });
-  }
-
-  // Set unauthorized.
-  return reply.status(401).send({ message: "Unauthorized" });
-};
-
-const postLogout = async (_request, reply) => {
-  reply
-    .setCookie(AUTH_COOKIE, null, {
-      // domain: "your.domain",
-      path: "/",
-      secure: false,
-      httpOnly: true,
-      sameSite: true,
-      maxAge: -1,
-    })
-    .status(200)
-    .send({ message: "Logout succeded." });
-};
-
-const attempt = async ({ email, password }) => {
-  const user = await new User().where("email", email).fetch({ require: false });
+const login = async ({ email, password }) => {
+  const user = await getUserByEmail(email);
 
   if (user && verify(password, user.get("password"))) {
     return {
@@ -62,16 +53,6 @@ const attempt = async ({ email, password }) => {
       created_at: user.get("created_at"),
       updated_at: user.get("updated_at"),
     };
-  }
-
-  return false;
-};
-
-const login = async ({ email, password }) => {
-  const user = await attempt({ email, password });
-
-  if (user) {
-    return user;
   }
 
   return false;

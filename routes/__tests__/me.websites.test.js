3fb1359486d27f6636fa15aef11bf17110dc0532
Renato Pozzi <r.pozzi@bluecube.it>
2021-09-12 18:49:56 +0200

    added two methods for ownership check

--- routes/__tests__/me.websites.test.js
+++ routes/__tests__/me.websites.test.js
@@ -1,46 +1,62 @@
+const dbInstance = require("../../lib/dbInstance");
 const { doLogin } = require("../../jest/doLogin.js");
-const { refreshDb } = require("../../jest/refreshDb.js");
 const { apiCall } = require("../../jest/apiCall");
-const { dbInstance } = require("../../lib/dbInstance");
-
-const websites = require("../../jest/fixtures/models/websites.json");
-
-let accessToken = null;
+const { mockerize, truncate } = require("../../lib/mockerize.js");
 
 beforeAll(async () => {
-  await refreshDb();
-  accessToken = await doLogin("info@renatopozzi.me", "password");
+  await truncate("websites");
+  await mockerize("users.json");
 });
+afterAll(async () => dbInstance.end());
 
-afterAll(async () => dbInstance.knex.destroy());
+const payloads = [
+  { id: 1, name: "Devpoint", url: "jalbum.net", shared: false },
+  { id: 2, name: "Jetpulse", url: "ed.gov", shared: false },
+  { id: 3, name: "Tekfly", url: "youku.com", shared: true },
+];
 
 describe("POST /me/websites", () => {
   it(`should return 401`, async () => {
     const response = await apiCall("POST", "/v2/me/websites");
     expect(response.statusCode).toBe(401);
   });
-});
 
-describe.each(websites)("POST /me/websites", (row) => {
-  it(`should return 200 ${row.url}`, async () => {
+  it.each(payloads)(`should return 200 %p`, async (payload) => {
+    const { id, ...rest } = payload;
+    const { accessToken, user } = await doLogin(
+      "rtroman0@hatena.ne.jp",
+      "password"
+    );
+
     const response = await apiCall("POST", "/v2/me/websites", {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
-      payload: row,
+      payload: rest,
     });
 
     expect(response.statusCode).toBe(200);
+    expect(typeof response.json()).toBe("object");
+    expect(response.json()).toHaveProperty("data");
+    expect(response.json().data).toMatchObject({
+      ...payload,
+      user_id: user.id,
+    });
   });
 });
 
 describe("GET /me/websites", () => {
   it("should return 401", async () => {
-    const response = await apiCall("GET", "/v2/me/websites");
+    const response = await apiCall("GET", "/v2/me/websites", {});
     expect(response.statusCode).toBe(401);
   });
 
   it("should return 200", async () => {
+    const { accessToken, user } = await doLogin(
+      "rtroman0@hatena.ne.jp",
+      "password"
+    );
+
     const response = await apiCall("GET", "/v2/me/websites", {
       headers: {
         Authorization: `Bearer ${accessToken}`,
@@ -48,5 +64,37 @@ describe("GET /me/websites", () => {
     });
 
     expect(response.statusCode).toBe(200);
+    expect(typeof response.json()).toBe("object");
+    expect(response.json()).toHaveProperty("data");
+    expect(Array.isArray(response.json().data)).toBe(true);
+    expect(response.json().data.length).toBe(payloads.length);
+    expect(response.json().data.every((el) => el.user_id === user.id)).toBe(
+      true
+    );
+  });
+});
+
+describe("GET /me/websites/:wid", () => {
+  it.each(payloads)("should return 401 %p", async (payload) => {
+    const wid = payload.id;
+    const response = await apiCall("GET", `/v2/me/websites/${wid}`, {});
+    expect(response.statusCode).toBe(401);
+  });
+
+  it.each(payloads)("should return 200 %p", async (payload) => {
+    const wid = payload.id;
+
+    const { accessToken } = await doLogin("rtroman0@hatena.ne.jp", "password");
+
+    const response = await apiCall("GET", `/v2/me/websites/${wid}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(typeof response.json()).toBe("object");
+    expect(response.json()).toHaveProperty("data");
+    expect(response.json().data).toMatchObject(payload);
   });
 });
--- routes/me/websites.js
+++ routes/me/websites.js
@@ -1,55 +1,60 @@
-const { Website } = require("../../lib/models");
-const { storeMeWebsitesOpts, putMeWebsiteOpts } = require("./opts");
-const { generate } = require("../../utils/seeds");
+const { format } = require("../../utils/response");
 const {
   getUserWebsites,
   getUserWebsite,
+  createWebsite,
   updateUserWebsite,
   deleteUserWebsite,
-  createUserWebsite,
 } = require("../../lib/db");
+
+const opts = {
+  schema: {
+    body: {
+      type: "object",
+      required: ["url", "name", "shared"],
+      properties: {
+        url: { type: "string" },
+        name: { type: "string" },
+        shared: { type: "boolean" },
+      },
+    },
+  },
+};
 
 const websites = (fastify, _opts, done) => {
   fastify.addHook("onRequest", (request) => request.jwtVerify());
 
-  fastify.get("/websites", async (request, _) => {
-    return getUserWebsites(request.user.data.id);
+  fastify.get("/websites", async (request) => {
+    const uid = request.user.data.id;
+    const websites = await getUserWebsites(uid);
+    return format(websites);
   });
 
-  fastify.get("/websites/:seed", async (request, reply) => {
-    const { seed } = request.params;
-
-    try {
-      const website = await getUserWebsite(request.user.data.id, seed);
-      website.shared = +website.shared;
-      return website;
-    } catch (err) {
-      return reply.status(404).send({ message: "Not found" });
-    }
+  fastify.get("/websites/:wid", async (request) => {
+    const uid = request.user.data.id;
+    const wid = request.params.wid;
+    const website = await getUserWebsite(uid, wid);
+    return format(website);
   });
 
-  fastify.put("/websites/:seed", putMeWebsiteOpts, async (request, _) => {
-    const { seed } = request.params;
-    const website = await updateUserWebsite(request.user.data.id, seed, {
-      ...request.body,
-    });
-
-    return { message: "Updated.", data: website };
+  fastify.put("/websites/:wid", opts, async (request) => {
+    const uid = request.user.data.id;
+    const wid = request.params.wid;
+    const website = await updateUserWebsite(uid, wid, request.body);
+    return format(website, { message: "Website updated." });
   });
 
-  fastify.delete("/websites/:seed", async (request, _) => {
-    const { seed } = request.params;
-    await deleteUserWebsite(request.user.data.id, seed);
-    return { message: "Deleted" };
+  fastify.delete("/websites/:wid", async (request) => {
+    const uid = request.user.data.id;
+    const wid = request.params.wid;
+    await deleteUserWebsite(uid, wid);
+    return format(null, { message: "Website deleted." });
   });
 
-  fastify.post("/websites", storeMeWebsitesOpts, async (request, _) => {
-    const seed = generate();
-
-    return createUserWebsite(request.user.data.id, {
-      seed,
-      ...request.body,
-    });
+  fastify.post("/websites", opts, async (request) => {
+    const uid = request.user.data.id;
+    const website = await createWebsite([...Object.values(request.body), uid]);
+    return format(website, { message: "Website created." });
   });
 
   done();

6a964af899f2ab6cf08087a0141a9fd55192e9c0
Renato Pozzi <r.pozzi@bluecube.it>
2021-09-20 18:38:32 +0200

    moved doLogin

--- utils/tests.js
+++ utils/tests.js
@@ -1,6 +1,30 @@
 const { build } = require("../app");
+const { getUserByEmail } = require("../lib/db");
 
 const apiCall = async (method, url, apiOpts, buildOpts = {}) =>
   build(buildOpts).inject({ method, url, ...apiOpts });
 
-module.exports = { apiCall };
+const doLogin = async (email, password) => {
+  const app = await build();
+  const response = await app.inject({
+    method: "POST",
+    url: "/v2/auth/login",
+    payload: {
+      email: email,
+      password: password,
+    },
+  });
+
+  if (response.statusCode === 200) {
+    const user = await getUserByEmail(email);
+
+    return {
+      accessToken: JSON.parse(response.body).access_token,
+      user: user,
+    };
+  }
+
+  return { accessToken: null, user: null };
+};
+
+module.exports = { apiCall, doLogin };
